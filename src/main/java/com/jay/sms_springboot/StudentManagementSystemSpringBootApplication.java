5e8147342b00c2a3ac3f79ed82833f8b7dd3c714
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-08 20:46:59 -0400

    Update

--- src/main/java/com/jay/sms_springboot/StudentManagementSystemSpringBootApplication.java
+++ src/main/java/com/jay/sms_springboot/StudentManagementSystemSpringBootApplication.java
@@ -3,11 +3,12 @@ package com.jay.sms_springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class StudentManagementSystemSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemSpringBootApplication.class, args);
-	}
 
+	}
 }
