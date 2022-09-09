d9f17908760c04712ce4af818c4a718478f21b0d
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-08 20:47:04 -0400

    Update

--- src/test/java/com/jay/sms_springboot/StudentControllerRepositoryRepositoryManagementSystemSpringBootApplicationTests.java
+++ src/test/java/com/jay/sms_springboot/StudentControllerRepositoryRepositoryManagementSystemSpringBootApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StudentManagementSystemSpringBootApplicationTests {
+class StudentControllerRepositoryRepositoryManagementSystemSpringBootApplicationTests {
 
 	@Test
 	void contextLoads() {
